68e06a62ccb7bbd8a9151a2f88e5f78a60c7c208
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-16 14:23:24 +0530

    Update 584.sql

--- 584.sql
+++ 584.sql
@@ -1,7 +1,12 @@
 #584. Find Customer Referee
 
+  Solution 1: 
 Select c1.name from
 Customer c1
 Left Join Customer c2
 On c1.id = c2.id
 Where c1.referee_id != 2 OR c1.referee_id IS NULL
+
+  Solution 2:
+select name from Customer
+where referee_id != 2 or referee_id is null
