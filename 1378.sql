0ce5bb8e9b1c998a06f55796198020f751c2ac5a
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-18 13:28:47 +0530

    Update 1378.sql

--- 1378.sql
+++ 1378.sql
@@ -1,3 +1,11 @@
 #1378.Replace Employee ID With The Unique Identifier
 
+select unique_id, name 
+from Employees LEFT JOIN EmployeeUNI USING(ID)
+
+#select unique_id ,name
+#from employees as emp
+#left join employeeUNI as uni
+#on uni.id = emp.id
+
 
