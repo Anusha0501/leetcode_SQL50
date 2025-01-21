cbf9eb018a853cdd9af2e4d9ffb21e3cad064550
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-21 22:03:49 +0530

    Create 1633.sql

---
+++ 1633.sql
@@ -0,0 +1,16 @@
+#1633. Percentage of Users Attended a Contest
+
+select
+r.contest_id,
+round(
+(count(r.contest_id) / (select count(*) from Users)) * 100
+, 2) as percentage
+from
+Users as u,
+Register as r
+where
+u.user_id = r.user_id
+group by r.contest_id
+order by
+percentage desc,
+contest_id asc;
