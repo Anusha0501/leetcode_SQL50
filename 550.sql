3544ed93ec71bf8b974e5db01b0d9081e606de2c
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-22 19:49:43 +0530

    Update 550.sql

--- 550.sql
+++ 550.sql
@@ -1,1 +1,8 @@
 #550. Game Play Analysis IV
+
+select
+round(sum(datediff(t2.event_date, t1.event_date) = 1) / count(distinct t2.player_id),2) as fraction
+from
+Activity t1 join Activity t2
+on
+t1.player_id = t2.player_id;
