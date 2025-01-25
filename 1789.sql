23b704472a2c655543612aa570242957fd1aa87c
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-25 17:20:51 +0530

    Create 1789.sql

---
+++ 1789.sql
@@ -0,0 +1,7 @@
+#1789. Primary Department for Each Employee
+
+select employee_id, department_id
+from Employee
+where primary_flag = 'Y' or employee_id in (
+select employee_id from employee group by employee_id having count(department_id) = 1)
+
