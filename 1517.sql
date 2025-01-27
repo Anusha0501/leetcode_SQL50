c9a49d8a13db9b3ea4cde6f66297412d3a873659
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-27 22:05:27 +0530

    Update 1517.sql

--- 1517.sql
+++ 1517.sql
@@ -1,10 +1,7 @@
 #1517. Find Users With Valid E-Mails
 
-SELECT user_id, name, mail
-FROM Users
-WHERE mail REGEXP '^[a-zA-Z]{1}[a-zA-Z0-9_.-]*@leetcode\.com$'
+select * from Users where right(mail, 13) like "@leetcode.com" and left(mail, length(mail)-13) rlike '^[A-Za-z][A-Za-z0-9._-]*$'
 
 #^[a-zA-Z] - The first character is a letter
 #[a-zA-Z0-9_.-]* - The accepted characters after the first repeated 0 or more times
 #@leetcode\.com$ - The email must end with the string '@leetcode.com'
-
