e3a401c39a3fd98173c356e1513fdf8235852879
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-02-01 13:49:15 +0530

    Create 1327.sql

---
+++ 1327.sql
@@ -0,0 +1,7 @@
+#1327. List the Products Ordered in a Period
+
+SELECT product_name,unit FROM Products JOIN (SELECT product_id,sum(unit) AS 'unit' FROM
+Orders
+WHERE month(order_date) = 2 AND year(order_date) = 2020
+GROUP BY product_id
+HAVING sum(unit) >=100)T ON Products.product_id = T.product_id
